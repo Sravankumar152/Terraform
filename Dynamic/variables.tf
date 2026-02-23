f004ab0f666a7e7b10f9f9cee05b09e1faaa7ca7
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-23 16:15:37 +0530

    pushing dynamic loop module

---
+++ Dynamic/variables.tf
@@ -0,0 +1,27 @@
+variable "ingress_rules" {
+
+    default = [
+        {
+            port        = 22
+            cidr_blocks      = ["0.0.0.0/0"]
+            description = "allow devices from internet"
+        },
+        
+        {
+
+            port        = 443
+            cidr_blocks      = ["0.0.0.0/0"]
+            description = "allow devices from internet"
+
+        },
+        
+        {
+
+            port        = 3306
+            cidr_blocks      = ["0.0.0.0/0"]
+            description = "allow devices from internet"
+
+        }
+    ]
+  
+}
