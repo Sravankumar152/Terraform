9130d139e3d764b25571d4aef00ea3f09be4a61a
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-23 16:57:44 +0530

    adding data resource

--- Data_Resources/dataresource.tf
+++ Data_Resources/dataresource.tf
@@ -17,3 +17,14 @@ data "aws_ami" "resource" {
     values = ["hvm"]
   }
 }
+
+
+data "aws_ami" "example" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.10.20260216.1-kernel-6.1-x86_64"]
+  }
+}
--- Data_Resources/output.tf
+++ Data_Resources/output.tf
@@ -1,6 +1,14 @@
 output "ec2_output" {
 
-    value = aws_instance.example
+    value = data.aws_ami.resource.id
+    description = "value"
+  
+}
+
+
+output "manualinstance" {
+
+    value = data.aws_ami.example
     description = "value"
   
 }
